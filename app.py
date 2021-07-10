f7648e1bca08595265114cdb0bb7fbde37affa20
Γεώργιος Σεμερτζάκης <gsemertzakis@hotmail.com>
2021-07-10 12:37:44 +0300

    setup application

--- app.py
+++ app.py
@@ -12,7 +12,31 @@ def home():
 
 @app.route('/predict', methods=['GET'])
 def predict():
-    float_features = [float(x) for x in request.form.values()]
+    fixed_acidity = request.args.get('fixed acidity')
+    volatile_acidity = request.args.get('volatile acidity')
+    citric_acid = request.args.get('citric acid')
+    residual_sugar = request.args.get('residual sugar')
+    chlorides = request.args.get('residual sugar')
+    free_sulfur_dioxide = request.args.get('free sulfur dioxide')
+    total_sulfur_dioxide = request.args.get('total sulfur dioxide')
+    density = request.args.get('density')
+    ph = request.args.get('pH')
+    sulphates = request.args.get('sulphates')
+    alcohol = request.args.get('alcohol')
+
+    features = [fixed_acidity,
+                volatile_acidity,
+                citric_acid,
+                residual_sugar,
+                chlorides,
+                free_sulfur_dioxide,
+                total_sulfur_dioxide,
+                density,
+                ph,
+                sulphates,
+                alcohol]
+
+    float_features = [float(x) for x in features]
     final_features = [np.array(float_features)]
     prediction = model.predict(final_features)
     output = prediction[0]
